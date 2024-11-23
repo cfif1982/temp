df9f250330e280391f29f88ec967f5e62ef944ce
Nastya <nastya@example.com>
2024-11-23 22:23:14 +0400

    add gitignore

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 struct Command
-<<<<<<< HEAD
 {
   string type;
   int value;
@@ -22,7 +21,12 @@ int main()
   string number_str = "";
   int num = 0;
 
-  for(int i = 0; i < str.size(); i++)
+  
+
+
+
+
+  /*for(int i = 0; i < str.size(); i++)
   {
     if(str[i] == ' ')
     {
@@ -39,7 +43,7 @@ int main()
         com[i].type = str.substr(i);
       }
     }
-  }
+  }*/
     
   //com.type = strCommand;
   //num = stoi(number_str); // здесь хранится значение
@@ -55,41 +59,6 @@ int main()
   //cout << "Значение:" << num << endl;
 
   system("pause");
-=======
-{
-  string type;
-  int value;
-} int main()
-{
-  SetConsoleOutputCP(CP_UTF8);
-
-  Command com;
-
-  string str = "move 50";
-  string strCommand = ""; // Здесь хранится команда
-  string number_str = "";
-  int num = 0;
-
-  for (size_t i = 0; i < str.size(); i++)
-  {
-    if (str[i] == ' ')
-    {
-      number_str = str.substr(i + 1);
-      // break;
-    }
-    else
-    {
-      strCommand += str[i];
-    }
-
-    com.type = strCommand;
-    num = stoi(number_str); // Здесь хранится значение
-    com.value = num;
-  }
-
-  cout << "Команда:" << com.type << endl;
-  cout << "Значение:" << com.value << endl;
->>>>>>> ba60191f826055b1b7924c9b4f9ae74f16d5e8f1
 
   return 0;
 }
