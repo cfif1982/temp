5d8e5b819a7ee6e543a3c68541858bb66b448d33
Nastya <nastya@example.com>
2024-12-06 22:10:50 +0400

    question number one

--- main.cpp
+++ main.cpp
@@ -9,19 +9,45 @@ struct Command
   string type;
   int value;
 };
-
-// Hello Nastya
-// add vetka01
 int main()
 {
   SetConsoleOutputCP(CP_UTF8);
 
-  Command com[10];
+  const int INDEX = 10; //количество элементов в массиве
+  string new_command = ";"; //отделяет команды
+  string type_or_value = " "; //отделяет команду и значение
+
+  Command commands[INDEX]; //массив команд 
 
   string str = "move 50;height 30;rotate 40";
-  // string strCommand = ""; // Здесь хранится команда
-  string number_str = "";
-  int num = 0;
+  //string number_str = "";
+  for(int j = 0; j < INDEX; ) //цикл для целой команды
+  {
+    for(int i = 0; i < str.size(); i++) //цикл пробегается по каждому символу строки и записывает отдельно команду и значение
+    {
+      if(str.substr(i, 1) != type_or_value)
+      {
+        commands[j].type = str.substr(i, 1);
+      } else if(str.substr(i, 1) == new_command)
+      {
+        j++;
+        break; //выходим из цикла, т.к. нашли следующую команду
+      } else if(str.substr(i, 1) == type_or_value)
+      {
+        commands[j].value = stoi(str.substr(i + 1)); //число записывается полностью
+      }
+    }
+  }
+    
+
+  /*
+  тебе нужно завести переменную, в которой ты будешь хранить индекс в массиве команд 
+  когда ты определилась с командой и ее параметрами, тогда создаешь саму команду и вставляешь ее в массив по нужному индексу
+  для определения начала команды и т.д. рекомендую завести переменную
+  пока подробности говорить не буду - сама подумай)))
+  ну всё пока, а то так весь код за тебя напишу)))
+  
+  */
 
   /*for(int i = 0; i < str.size(); i++)
   {
@@ -46,14 +72,11 @@ int main()
   // num = stoi(number_str); // здесь хранится значение
   // com.value = num;
 
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < INDEX; i++)
   {
-    cout << "Команда:" << com[i].type << endl;
-    cout << "Значение:" << com[i].value << endl;
+    cout << "Команда:" << commands[i].type << endl;
+    cout << "Значение:" << commands[i].value << endl;
   }
-
-  // cout << "Команда:" << command << endl;
-  // cout << "Значение:" << num << endl;
 
   system("pause");
 
