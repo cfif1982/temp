32bb777fae00de5246cc31d5934b502b711ab606
cfif <s_filip@mail.ru>
2024-12-07 17:11:59 +0400

    sasha_01

--- main.cpp
+++ main.cpp
@@ -13,66 +13,44 @@ int main()
 {
   SetConsoleOutputCP(CP_UTF8);
 
-  const int INDEX = 10; //количество элементов в массиве
-  string new_command = ";"; //отделяет команды
-  string type_or_value = " "; //отделяет команду и значение
+  const int INDEX = 10;     // количество элементов в массиве
+  string new_command = ";"; // отделяет команды
+  string probel = " ";      // отделяет команду и значение
 
-  Command commands[INDEX]; //массив команд 
+  Command commands[INDEX]; // массив команд
 
-  string str = "move 50;height 30;rotate 40";
-  //string number_str = "";
-  for(int j = 0; j < INDEX; ) //цикл для целой команды
+  string str = "move 50;height 30;rotate 40;";
+  int j = 0;
+  bool flag_type = true;
+  string temp = "";
+
+  for (int i = 0; i < str.size(); i++) // цикл пробегается по каждому символу строки и записывает отдельно команду и значение
   {
-    for(int i = 0; i < str.size(); i++) //цикл пробегается по каждому символу строки и записывает отдельно команду и значение
+    if (str.substr(i, 1) == new_command)
     {
-      if(str.substr(i, 1) != type_or_value)
+      commands[j].value = stoi(temp);
+      temp = "";
+      flag_type = true;
+      j++;
+    }
+    else if (str.substr(i, 1) == probel)
+    {
+      flag_type = false;
+    }
+    else
+    {
+      if (flag_type == true)
       {
-        commands[j].type = str.substr(i, 1);
-      } else if(str.substr(i, 1) == new_command)
+        commands[j].type = commands[j].type + str.substr(i, 1);
+      }
+      else
       {
-        j++;
-        break; //выходим из цикла, т.к. нашли следующую команду
-      } else if(str.substr(i, 1) == type_or_value)
-      {
-        commands[j].value = stoi(str.substr(i + 1)); //число записывается полностью
+        temp += str.substr(i, 1);
       }
     }
   }
-    
 
-  /*
-  тебе нужно завести переменную, в которой ты будешь хранить индекс в массиве команд 
-  когда ты определилась с командой и ее параметрами, тогда создаешь саму команду и вставляешь ее в массив по нужному индексу
-  для определения начала команды и т.д. рекомендую завести переменную
-  пока подробности говорить не буду - сама подумай)))
-  ну всё пока, а то так весь код за тебя напишу)))
-  
-  */
-
-  /*for(int i = 0; i < str.size(); i++)
-  {
-    if(str[i] == ' ')
-    {
-      do
-      {
-        number_str = str.substr(i + 1); //ограничить значение до ;
-      } while(str[i] != ';');
-    } else if(str[i] == ';')
-    {
-      com[i].value = stoi(str.substr(i + 2));
-    } else{
-      while(str.substr(i) != ";")
-      {
-        com[i].type = str.substr(i);
-      }
-    }
-  }*/
-
-  // com.type = strCommand;
-  // num = stoi(number_str); // здесь хранится значение
-  // com.value = num;
-
-  for (int i = 0; i < INDEX; i++)
+  for (int i = 0; i < j; i++)
   {
     cout << "Команда:" << commands[i].type << endl;
     cout << "Значение:" << commands[i].value << endl;
